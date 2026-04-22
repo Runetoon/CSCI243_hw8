421e7e2bc8be2897ac3dcb591d51ba120bad9ae9
sfr8408 <sam3.rizzico@icloud.com>
2026-04-22 19:40:21 -0400

    iitial commit

---
+++ quicksort.c
@@ -0,0 +1,17 @@
+//
+//
+// C Version: c99
+// File: quicksort.c
+// quicksort.c is a program that cann quicksort via recursion or threads
+//
+// @author sfr8408: Sam Rizzico
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+
+	return EXIT_SUCCESS;
+}
+
