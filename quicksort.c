0255fff1d803bd55b17e4496d337713f41e606b0
sfr8408 <sam3.rizzico@icloud.com>
2026-04-22 22:56:35 -0400

    git commit and wording fix

--- quicksort.c
+++ quicksort.c
@@ -155,7 +155,7 @@ int main(int argc, char **argv){
 		filename = argv[2];
 	} 
 	else{
-		fprintf(stderr, "Usage: %s [-p] file_of_integers\n", argv[0]);
+		fprintf(stderr, "usage: %s [-p] file_of_integers\n", argv[0]);
 		return 1;
 	}
 	FILE *fp = fopen(filename, "r");
